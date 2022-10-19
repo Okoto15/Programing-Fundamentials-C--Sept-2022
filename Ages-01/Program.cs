6b1aecc4f1474ecefd6344eb937edcd46d15efc3
Okoto15 <stefan_em@abv.bg>
2022-10-19 17:22:08 +0300

    Create Program.cs

---
+++ Ages-01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net.Security;
+using System.Numerics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int personsAge = int.Parse(Console.ReadLine());
+
+        if (personsAge >= 0 && personsAge <= 2)
+        {
+            Console.WriteLine("baby");
+        }
+        else if (personsAge > 2 && personsAge <= 13)
+        {
+            Console.WriteLine("child");
+        }
+        else if (personsAge > 13 && personsAge <= 19)
+        {
+            Console.WriteLine("teenager");
+        }
+        else if (personsAge > 19 && personsAge <= 65)
+        {
+            Console.WriteLine("adult");
+        }
+        else if (personsAge >= 66)
+        {
+            Console.WriteLine("elder");
+        }
+    }
+}
